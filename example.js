c4c43fd68cfb26d68bde99b2c96a951ab48afb0f
Paul <go2mailshan@gmail.com>
2015-06-11 21:58:23 +0530

    providing an example.js

---
+++ example.js
@@ -0,0 +1,62 @@
+//inheriting Person from ValidationModel
+var Person = function (name,age) {
+	ValidationModel.apply(this,arguments);
+	this.name = name;
+	this.age = age;
+	this.email = null;
+};
+
+Person.prototype = Object.create(ValidationModel.prototype);
+
+Person.prototype.validations={
+	name:{
+		rules:['required'],
+		controlName:"Name"
+	},
+	age:{
+		rules:['number'],
+		controlName:"Age"
+	}
+}
+
+
+//inheriting adult from Person
+var Adult = function (name, age, email) {
+	Person.apply(this,arguments);
+	this.email = email;
+}
+
+Adult.prototype = Object.create(Person.prototype);
+
+Adult.prototype.validations={
+	email:{
+		rules:['required','customEmail'],
+		customEmail:function (obj, email) {
+			if(!email || email.length<6)
+				return{
+					code:1111,
+					msg:"email should be longer than 6 char"
+				}
+		},
+		controlName:"Email"
+	}
+}
+
+
+//inheriting unknown person from Person
+var UnknownPerson = function (name, age, country) {
+	Person.apply(this,arguments);
+	this.foundIn = country;
+}
+
+UnknownPerson.prototype = Object.create(Person.prototype);
+
+UnknownPerson.prototype.validations={
+	email:null,
+	name:null,
+	foundIn:{
+		rules : ["required"],
+		messages : ["Please provide @controlName"],
+		controlName : "Found in country"
+	}
+}
