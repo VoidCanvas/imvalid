ed162e25f9231ef99ea4fd4b31f8860807886236
metalshan <go2mailshan@gmail.com>
2016-03-12 02:38:11 +0530

    updating imvalid

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
 # imvalid
-A useful javascript validation model 
+This is a easy to use plugin to validate your JavaScript models (POJO). Check the examples to get an idea.
+
+## Install the plugin
+If you are using it in your node project you can run the *npm install* to get this plugin
+
+	$ npm install imvalid
+
+If you are not using it in node project than also you can install it through *npm* or get it from github repo and use the file *imvalid.js*
+
+
+## Model design with imvalid
+
+	var ValidationModel = require('imvalid'); //for client side use, you can autometically get this object in your global scope
+	//inheriting Person from ValidationModel
+	var Person = function (name,age) {
+		ValidationModel.apply(this,arguments);
+		this.name = name;
+		this.age = age;
+		this.email = null;
+	};
+
+	Person.prototype = Object.create(ValidationModel.prototype);
+
+	Person.prototype.validations={
+		name:{
+			rules:['required'],
+			controlName:"Name"
+		},
+		age:{
+			rules:['number'],
+			controlName:"Age"
+		}
+	}
+
+
+	//inheriting adult from Person
+	var Adult = function (name, age, email) {
+		Person.apply(this,arguments);
+		this.email = email;
+	}
+
+	Adult.prototype = Object.create(Person.prototype);
+
+	Adult.prototype.validations={
+		email:{
+			rules:['required','customEmail'],
+			customEmail:function (obj, email) {
+				if(!email || email.length<6)
+					return{
+						code:1111,
+						msg:"email should be longer than 6 char"
+					}
+			},
+			controlName:"Email"
+		}
+	}
+
+
+	//inheriting unknown person from Person
+	var UnknownPerson = function (name, age, country) {
+		Person.apply(this,arguments);
+		this.foundIn = country;
+	}
+
+	UnknownPerson.prototype = Object.create(Person.prototype);
+
+	UnknownPerson.prototype.validations={
+		email:null, //removing the validation for email. Base class had it
+		name:null, //removing the validation for name. Base class had it
+		foundIn:{
+			rules : ["required"],
+			messages : ["Please provide @controlName"],
+			controlName : "Found in country"
+		}
+	}
+
+
+## Using this model
+
+	var me = new Person("Paul");
+	me.validate(); //validate me
+	if(me.isValid){
+		console.log("me is valid");
+	}
+	else{
+		console.log("me is invalid");
+		console.log(me.validationErrors); // this is an array of validation errors
+	}
+
+
+	var he = new UnknownPerson("Shan");
+	he.validate(); //validate he
+	if(he.isValid){
+		console.log("he is valid");
+	}
+	else{
+		console.log("he is invalid");
+		console.log(he.validationErrors); // this is an array of validation errors
+	}
+
---
+++ imvalid-node.js
@@ -0,0 +1,129 @@
+
+
+var defaultIsvalid = false; //this is the default value of isValid. Before calling the .validate() function
+
+//core ValidationModel constructor
+var ValidationModel = function () {
+	this.isValid = false;
+};
+
+//validation error constructor.
+var ValidationError = function (controlName, msg, code) {
+	if(!controlName)
+		controlName="";
+
+	if(!msg)
+		throw new Error("Validation model: error message not passed");
+
+	this.code=code;
+	this.msg=msg.replace(/@controlName/g,controlName);
+};
+
+//default validation rules are set here 
+var coreValidationRules = {
+	____custom____:function (obj, property, controlName, func) {
+		var errorObj = func(obj, property);
+		if(errorObj){
+			var error = new ValidationError(controlName, errorObj.msg, errorObj.code);
+			return error;
+		}
+		return null;
+	},
+	required:function (property, controlName, msg) {
+		var code = 100;
+		if(!msg)
+			msg = "@controlName is required";
+
+		if(!property || !property.toString().trim()) //if nothing is inserted
+			return new ValidationError(controlName, msg, code);
+		
+		return null;
+	},
+	number:function (property, controlName, msg) {
+		var code = 101;
+		if(!msg)
+			msg = "@controlName must be a number";
+
+		if(property)
+			if(typeof property != "number")
+				return new ValidationError(controlName, msg, code);
+
+		return null;
+	}
+};
+
+//to clear errors
+ValidationModel.prototype.clearErrors = function() {
+	this.validationErrors = []; //clear the validation errors
+	this.isValid=defaultIsvalid;
+};
+
+//the validate function validates all properties and set the value of isValid
+ValidationModel.prototype.validate = function() {
+	this.clearErrors();
+	var validations = helper.getValidationRules(this); //to get proper validation rules set
+	for(var validation in validations){
+		if(validations.hasOwnProperty(validation)){
+			var rulesObj = validations[validation];
+			var property = this[validation];
+			var errorList = helper.validateRules(this,property,rulesObj); //this function will validate and assign all errors to the validationErrors				
+		}
+	}
+	if(!this.validationErrors.length)
+		this.isValid=true;
+
+	return this.isValid;
+};
+
+//helper
+var helper={
+	//to merge two objects and create the 3rd one
+	merge:function (obj1,obj2){
+	    var obj3 = {};
+	    for (var attrname in obj1) {
+	    	if(obj1.hasOwnProperty(attrname))
+		    	obj3[attrname] = obj1[attrname]; 
+	 	}
+	    for (var attrname in obj2) { 
+	    	if(obj2.hasOwnProperty(attrname))
+	    		obj3[attrname] = obj2[attrname]; 
+	    }
+	    return obj3;
+	},
+
+	getValidationRules:function (obj) {
+		var rulesSet = {};
+		if(obj){
+			var currentValidations = obj.validations || {};
+			rulesSet = this.merge(this.getValidationRules(obj.__proto__), currentValidations)			
+		}
+		return rulesSet;
+	},
+
+	validateRules:function (obj, property, rulesObj) {
+		var errorList = [];
+		if(rulesObj){
+			var validationRules = rulesObj.rules;
+			if(validationRules && validationRules.length){
+				validationRules.forEach(function (rule,i) {
+					if(rulesObj[rule]){
+						var error = coreValidationRules.____custom____(obj,property,rulesObj.controlName,rulesObj[rule]);
+						if(error)
+							obj.validationErrors.push(error);
+					}
+					else
+						if(coreValidationRules[rule]){
+							var msg = rulesObj["messages"] && rulesObj["messages"][i] 
+							var error = coreValidationRules[rule](property,rulesObj.controlName,msg);
+							if(error)
+								obj.validationErrors.push(error);
+						}
+				});
+			}
+		}
+		return errorList;
+	}
+}
+
+module.exports = ValidationModel;
+
